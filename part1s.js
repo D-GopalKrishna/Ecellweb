f1cead35fa20d5cd28c1a3e42986d3dc83de1229
D-GopalKrishna <59233227+D-GopalKrishna@users.noreply.github.com>
2020-03-11 07:32:23 +0530

    Add files via upload

---
+++ part1s.js
@@ -0,0 +1,74 @@
+alert("welcome to Our Website")
+var a = prompt("Enter your First Name")
+var b = prompt("Enter your Last Name")
+var c = prompt("Enter your Age")
+var d = prompt("Enter your Height in cm")
+var e = prompt("Enter your Petname")
+
+if((a[0]==='J') && (b[0] ==='J') && 20<c && c<30 && d>169 && e[e.length -1]==='y')
+{
+	console.log("Congrats, You passed the Spy Test")
+
+}
+
+else{
+	console.log("Sorry, nothing foy you here")
+}
+
+
+
+
+
+
+
+
+
+
+// var f = null;
+// var g = null;
+// var h = null;
+// var i = null;
+
+
+// if(a[0] === b[0]){
+// 	f = true;
+// }
+// else{
+// 	f = false;
+// }
+
+
+
+
+// if(c > 20 && c < 30){
+// 	g = true;
+// }
+// else{
+// 	g = false;
+// }
+
+
+
+// if(d >= 170){
+// 	g = true;
+// }
+//  else{
+// 	g = false;
+// }
+
+
+// if(e[e.length - 1] === "y"){
+// 	h = true;
+// }
+// else{
+// 	h = false;
+// }
+
+
+
+// if(f && g && h && i ){
+// 	console.log("Congrats, You passed the Spy Test");
+// }
+// else{
+// 	console.log("Sorry nothing for you here");
+// }
